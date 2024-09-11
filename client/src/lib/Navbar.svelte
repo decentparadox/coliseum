81fec4565dd5a6ef61e052dcdf3386a9b965d970
decentparadox <rsasank69@gmail.com>
2024-09-11 22:48:56 +0530

    frontend ⇒ client; added tailwind

---
+++ client/src/lib/Navbar.svelte
@@ -0,0 +1,13 @@
+<script>
+    let src = "/favicon.png"
+</script>
+<div class="flex justify-between items-center">
+<div class="w-8 h-8">
+    <img {src} alt="">
+</div>
+<div class="flex gap-4 ">
+    <p>Scoreboard</p>
+    <p>Challenges</p>
+    <p>Register</p>
+</div>
+</div>
